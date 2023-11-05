2ca75f1f9397730fab1b9cb93792a215d9bb0d5b
maverickPrakash <Prakashtheryon@gmail.com>
2023-11-05 16:05:12 -0500

    week 09

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Full Stack Development 1 </h1>
+        <h2>React Js  Programming week 9 lab exercise</h2>
+        <h3>101341685</h3>
+        <h4>Prakash Kumar</h4>
+        <h5>George Brown College</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
